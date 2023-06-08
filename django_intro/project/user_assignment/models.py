b87bd33eb93cde25336342758a2bf4cfefbe6934
SambitKumarNanda <nanu2647542@gmail.com>
2023-06-08 14:25:01 +0530

    many to many relationship added in user_assignment app

--- django_intro/project/user_assignment/models.py
+++ django_intro/project/user_assignment/models.py
@@ -16,9 +16,9 @@ class UserModel(models.Model):
 class UserProfileModel(models.Model):
     user_info = models.OneToOneField(UserModel , on_delete=models.CASCADE, blank=True, related_name="user_profile_model_user_info")
     bio = models.CharField(max_length=200, null=True, blank=True)
-    followers = models.IntegerField(null=True, blank=True)
+    followers = models.ManyToManyField(UserModel,related_name="user_profile_followers",blank=True)
     posts = models.IntegerField(null=True, blank=True)
-    following = models.IntegerField(null=True, blank=True)
+    following = models.ManyToManyField(UserModel,related_name="user_profile_following",blank=True)
     
     created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     
@@ -30,6 +30,7 @@ class UserPostModel(models.Model):
     title = models.CharField(max_length=50, null=True, blank=True)
     description = models.CharField(max_length=150, null=True, blank=True)
     user_detail_profile = models.ForeignKey(UserProfileModel, on_delete=models.CASCADE, blank=True, null=True, related_name="user_post_model_post_info")
+    likes = models.ManyToManyField(UserModel,related_name="user_post_likes", blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     
